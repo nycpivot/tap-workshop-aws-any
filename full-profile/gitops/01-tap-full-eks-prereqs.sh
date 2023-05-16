70438e253ac7c697a4d798feff7f2f5685ad3a44
Michael James <mijames@vmware.com>
2023-05-16 19:54:26 -0400

    Initial commit.

--- full-profile/gitops/01-tap-full-eks-prereqs.sh
+++ full-profile/gitops/01-tap-full-eks-prereqs.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
+read -p "Cluster Name: " cluster_name
 read -p "Full Domain Name: " full_domain
 
 echo export FULL_DOMAIN=$full_domain >> $HOME/.bashrc
 
-source $HOME/.bashrc #reload environment variables (still doesn't work in other scripts)
+source $HOME/.bashrc #reload environment variables (still can't be read from other scripts)
 
 #create a tmp file for now
 echo $full_domain > /tmp/tap-full-domain
 
-export EKS_CLUSTER_NAME=tap-full
 export TANZU_CLI_NO_INIT=true
 export TANZU_VERSION=v0.28.1
 export TAP_VERSION=1.5.2-build.1
 
-export CLI_FILENAME=tanzu-framework-linux-amd64-v0.28.1.tar
+export CLI_FILENAME=tanzu-framework-linux-amd64-v0.28.1.1.tar
 export ESSENTIALS_FILENAME=tanzu-cluster-essentials-linux-amd64-1.5.0.tgz
 
 export PIVNET_USERNAME=$(aws secretsmanager get-secret-value --secret-id tap-workshop | jq -r .SecretString | jq -r .\"pivnet-username\")
@@ -24,7 +24,10 @@ export PIVNET_TOKEN=$(aws secretsmanager get-secret-value --secret-id tap-worksh
 token=$(curl -X POST https://network.pivotal.io/api/v2/authentication/access_tokens -d '{"refresh_token":"'$PIVNET_TOKEN'"}')
 access_token=$(echo $token | jq -r .access_token)
 
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $access_token" -X GET https://network.pivotal.io/api/v2/authentication
+curl -i -H "Accept: application/json" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $access_token" \
+    -X GET https://network.pivotal.io/api/v2/authentication
 
 
 # 1. CREATE CLUSTER
@@ -32,18 +35,18 @@ echo
 echo "<<< CREATING CLUSTER >>>"
 echo
 
-eksctl create cluster --name $EKS_CLUSTER_NAME --managed --region $AWS_REGION --instance-types t3.xlarge --version 1.25 --with-oidc -N 3
+eksctl create cluster --name $cluster_name --managed --region $AWS_REGION --instance-types t3.xlarge --version 1.25 --with-oidc -N 3
 
 rm .kube/config
 
 #configure kubeconfig
 arn=arn:aws:eks:$AWS_REGION:$AWS_ACCOUNT_ID:cluster
 
-aws eks update-kubeconfig --name $EKS_CLUSTER_NAME --region $AWS_REGION
+aws eks update-kubeconfig --name $cluster_name --region $AWS_REGION
 
-kubectl config rename-context $arn/$EKS_CLUSTER_NAME $EKS_CLUSTER_NAME
+kubectl config rename-context $arn/$cluster_name $cluster_name
 
-kubectl config use-context $EKS_CLUSTER_NAME
+kubectl config use-context $cluster_name
 
 
 # 2. INSTALL CSI PLUGIN (REQUIRED FOR K8S 1.23+)
@@ -51,17 +54,17 @@ echo
 echo "<<< INSTALLING CSI PLUGIN >>>"
 echo
 
-rolename=$EKS_CLUSTER_NAME-csi-driver-role
+rolename=$cluster_name-csi-driver-role
 
 #https://docs.aws.amazon.com/eks/latest/userguide/managing-ebs-csi.html
 aws eks create-addon \
-    --cluster-name $EKS_CLUSTER_NAME \
+    --cluster-name $cluster_name \
     --addon-name aws-ebs-csi-driver \
     --service-account-role-arn "arn:aws:iam::$AWS_ACCOUNT_ID:role/$rolename" \
     --no-cli-pager
 
 #https://docs.aws.amazon.com/eks/latest/userguide/csi-iam-role.html
-oidc_id=$(aws eks describe-cluster --name $EKS_CLUSTER_NAME --query "cluster.identity.oidc.issuer" --output text | awk -F '/' '{print $5}')
+oidc_id=$(aws eks describe-cluster --name $cluster_name --query "cluster.identity.oidc.issuer" --output text | awk -F '/' '{print $5}')
 
 # Check if a IAM OIDC provider exists for the cluster
 # https://docs.aws.amazon.com/eks/latest/userguide/enable-iam-roles-for-service-accounts.html
@@ -72,7 +75,7 @@ if [[ -z $(aws iam list-open-id-connect-providers | grep $oidc_id) ]]; then
     exit 1
     fi
 
-    eksctl utils associate-iam-oidc-provider --cluster $EKS_CLUSTER_NAME --approve
+    eksctl utils associate-iam-oidc-provider --cluster $cluster_name --approve
 fi
 
 cat <<EOF | tee aws-ebs-csi-driver-trust-policy.json
@@ -118,7 +121,7 @@ echo
 echo "<<< CREATING IAM ROLES FOR ECR >>>"
 echo
 
-export OIDCPROVIDER=$(aws eks describe-cluster --name $EKS_CLUSTER_NAME --region $AWS_REGION | jq '.cluster.identity.oidc.issuer' | tr -d '"' | sed 's/https:\/\///')
+export OIDCPROVIDER=$(aws eks describe-cluster --name $cluster_name --region $AWS_REGION | jq '.cluster.identity.oidc.issuer' | tr -d '"' | sed 's/https:\/\///')
 
 cat <<EOF > build-service-trust-policy.json
 {
@@ -325,24 +328,16 @@ echo
 echo "<<< INSTALLING TANZU AND CLUSTER ESSENTIALS >>>"
 echo
 
-export IMGPKG_REGISTRY_HOSTNAME_0=registry.tanzu.vmware.com
-export IMGPKG_REGISTRY_USERNAME_0=$PIVNET_USERNAME
-export IMGPKG_REGISTRY_PASSWORD_0=$PIVNET_PASSWORD
-export IMGPKG_REGISTRY_HOSTNAME_1=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
-export IMGPKG_REGISTRY_USERNAME_1=AWS
-export IMGPKG_REGISTRY_PASSWORD_1=`aws ecr get-login-password --region $AWS_REGION`
 export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
 export INSTALL_REGISTRY_USERNAME=$PIVNET_USERNAME
 export INSTALL_REGISTRY_PASSWORD=$PIVNET_PASSWORD
-
-export TARGET_TAP_REPO=tap-images
-export TARGET_TBS_REPO=tap-build-service
 export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:79abddbc3b49b44fc368fede0dab93c266ff7c1fe305e2d555ed52d00361b446
 
 rm -rf $HOME/tanzu
 mkdir $HOME/tanzu
 
-wget https://network.pivotal.io/api/v2/products/tanzu-application-platform/releases/1295414/product_files/1478717/download --header="Authorization: Bearer $access_token" -O $HOME/tanzu/$CLI_FILENAME
+wget https://network.pivotal.io/api/v2/products/tanzu-application-platform/releases/1295414/product_files/1478717/download \
+    --header="Authorization: Bearer $access_token" -O $HOME/tanzu/$CLI_FILENAME
 tar -xvf $HOME/tanzu/$CLI_FILENAME -C $HOME/tanzu
 
 cd tanzu
@@ -359,7 +354,8 @@ cd $HOME
 rm -rf $HOME/tanzu-cluster-essentials
 mkdir $HOME/tanzu-cluster-essentials
 
-wget https://network.pivotal.io/api/v2/products/tanzu-cluster-essentials/releases/1275537/product_files/1460876/download --header="Authorization: Bearer $access_token" -O $HOME/tanzu-cluster-essentials/$ESSENTIALS_FILENAME
+wget https://network.pivotal.io/api/v2/products/tanzu-cluster-essentials/releases/1275537/product_files/1460876/download \
+    --header="Authorization: Bearer $access_token" -O $HOME/tanzu-cluster-essentials/$ESSENTIALS_FILENAME
 tar -xvf $HOME/tanzu-cluster-essentials/$ESSENTIALS_FILENAME -C $HOME/tanzu-cluster-essentials
 
 cd $HOME/tanzu-cluster-essentials
@@ -371,8 +367,6 @@ sudo cp $HOME/tanzu-cluster-essentials/imgpkg /usr/local/bin/imgpkg
 
 cd $HOME
 
-docker login $IMGPKG_REGISTRY_HOSTNAME_0 -u $IMGPKG_REGISTRY_USERNAME_0 -p $IMGPKG_REGISTRY_PASSWORD_0
-
 rm $HOME/tanzu/$CLI_FILENAME
 rm $HOME/tanzu-cluster-essentials/$ESSENTIALS_FILENAME
 
@@ -382,26 +376,20 @@ echo
 echo "<<< IMPORTING TAP PACKAGES >>>"
 echo
 
-tap_images_ecr=$(aws ecr describe-repositories --query "repositories[?repositoryName=='tap-images'].repositoryName" --output text)
-tap_build_service_ecr=$(aws ecr describe-repositories --query "repositories[?repositoryName=='tap-build-service'].repositoryName" --output text)
+acr_secret=$(aws secretsmanager get-secret-value --secret-id tap-workshop | jq -r .SecretString | jq -r .\"acr-secret\")
 
-aws ecr get-login-password --region $AWS_REGION | docker login --username $IMGPKG_REGISTRY_USERNAME_1 --password-stdin $IMGPKG_REGISTRY_HOSTNAME_1
+export IMGPKG_REGISTRY_HOSTNAME_0=registry.tanzu.vmware.com
+export IMGPKG_REGISTRY_USERNAME_0=$PIVNET_USERNAME
+export IMGPKG_REGISTRY_PASSWORD_0=$PIVNET_PASSWORD
+export IMGPKG_REGISTRY_HOSTNAME_1=tanzuapplicationregistry.azurecr.io
+export IMGPKG_REGISTRY_USERNAME_1=tanzuapplicationregistry
+export IMGPKG_REGISTRY_PASSWORD_1=$acr_secret
+export INSTALL_REPO=tanzu-application-platform/tap-packages
 
-if [[ -z $tap_images_ecr ]]
-then
-  aws ecr create-repository --repository-name $TARGET_TAP_REPO --region $AWS_REGION --no-cli-pager
-fi
+docker login $IMGPKG_REGISTRY_HOSTNAME_0 -u $IMGPKG_REGISTRY_USERNAME_0 -p $IMGPKG_REGISTRY_PASSWORD_0
 
-images_count=$(aws ecr describe-images --repository-name $TARGET_TAP_REPO | jq .imageDetails | jq length)
-if [[ $images_count = 0 ]]
-then
-  imgpkg copy --concurrency 1 -b $IMGPKG_REGISTRY_HOSTNAME_0/tanzu-application-platform/tap-packages:$TAP_VERSION --to-repo $IMGPKG_REGISTRY_HOSTNAME_1/$TARGET_TAP_REPO
-fi
-
-if [[ -z $tap_build_service_ecr ]]
-then
-  aws ecr create-repository --repository-name $TARGET_TBS_REPO --region $AWS_REGION --no-cli-pager
-fi
+imgpkg copy --concurrency 1 -b $IMGPKG_REGISTRY_HOSTNAME_0/tanzu-application-platform/tap-packages:${TAP_VERSION} \
+    --to-repo ${IMGPKG_REGISTRY_HOSTNAME_1}/$INSTALL_REPO
 
 #GITOPS
 rm sops
@@ -415,4 +403,4 @@ rm -rf tanzu-java-web-app
 git clone https://github.com/nycpivot/tanzu-java-web-app
 
 #INSTALL OOTB SUPPLY CHAIN - BASIC
-bash $HOME/tap-workshop-aws/full-profile/gitops/supply-chain/01-ootb-basic.sh
+bash $HOME/tap-workshop-aws-any/full-profile/gitops/supply-chain/01-ootb-basic.sh
